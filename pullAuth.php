3907724c974f2fe3c9fdea37232ab7917766bc44
Suetena <suetena_coder@outlook.com>
2022-05-02 10:31:30 +1300

    Update pullAuth.php

--- pullAuth.php
+++ pullAuth.php
@@ -3,7 +3,7 @@
 // echo '<a href="./">Home</a>'; 
 echo '<hr/><h5>REPO INFO</h5><hr/>';
 
-$outresults = system('git pull /var/www/html/devpro/d_amata.sfll.ws',$rv);
+$outresults = system('pa.sh',$rv);
 // echo "\n".'you got pull'."\n";
 echo '<pre>out: '.$outresults."\n".$rv.'</pre>';
 
