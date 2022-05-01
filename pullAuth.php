6d780b61e5ac37810e8f3603a9c2a10e7fee0f31
Suetena <suetena_coder@outlook.com>
2022-05-02 08:44:24 +1300

    Update pullAuth.php

--- pullAuth.php
+++ pullAuth.php
@@ -1,5 +1,12 @@
 <?php 
 
-echo '<a href="./">Home</a>'; 
-exec('git pull');
+// echo '<a href="./">Home</a>'; 
+echo '<hr/><h5>REPO INFO</h5><hr/>';
+$code = null;
+$output = null;
+$cmdCall = 'git pull';
+
+exec($cmdCall, $output, $code);
 echo 'you got pull';
+print_r($output);
+
